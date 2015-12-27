d42d3d6f701683cd7bac5c0d29b616d0408f3ce6
One Zero <zeroonegit@gmail.com>
2015-12-27 14:20:37 +0800

    计算实数和复数平方根

--- num_sqrt.py
+++ num_sqrt.py
@@ -3,6 +3,12 @@
 # author by: One Zero
 # Filename: num_sqrt.py
 
-num = float(input('请输入一个数字： '))
-num_sqrt = num ** 0.5
-print(' %0.3f 的平方根为 %0.3f' % (num, num_sqrt))
+#num = float(input('请输入一个数字： '))
+#num_sqrt = num ** 0.5
+#print(' %0.3f 的平方根为 %0.3f' % (num, num_sqrt))
+
+import cmath
+
+num = int(input('请输入一个数字： '))
+num_sqrt = cmath.sqrt(num)
+print('{0} 的平方根为 {1:0.3f}+{2:0.3f}j'.format(num, num_sqrt.real, num_sqrt.imag))
