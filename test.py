39c1b6a190f3a5512b0504c2faf69c88a7e8ce6e
One Zero <zeroonegit@gmail.com>
2015-12-27 14:07:26 +0800

    将运算合并为一行代码

--- test.py
+++ test.py
@@ -1,14 +1,16 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-# Filename: test.py
 # author by: One Zero
+# Filename: test.py
 
-# 用户输入数字
-num1 = input('输入第一个数字: ')
-num2 = input('输入第二个数字: ')
+## 用户输入数字
+#num1 = input('输入第一个数字: ')
+#num2 = input('输入第二个数字: ')
+#
+## 求和
+#sum = float(num1) + float(num2)
+#
+## 显示计算结果
+#print('数字 {0} 和 {1} 相加结果为： {2}'.format(num1, num2, sum))
 
-# 求和
-sum = float(num1) + float(num2)
-
-# 显示计算结果
-print('数字 {0} 和 {1} 相加结果为： {2}'.format(num1, num2, sum))
+print('两数之和为 %.1f' % (float(input('输入第一个数字： ')) + float(input('输入第二个数字： '))))
