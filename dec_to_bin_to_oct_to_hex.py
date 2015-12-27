918326723753d6a6cc9381a292e8f53167caf842
One Zero <zeroonegit@gmail.com>
2015-12-27 21:11:00 +0800

    十进制转二进制 、八进制、十六进制

---
+++ dec_to_bin_to_oct_to_hex.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+############################
+# File Name: dec_to_bin_to_oct_to_hex.py
+# Author: One Zero
+# Mail: zeroonegit@gmail.com
+# Created Time: 2015-12-27 21:02:48
+############################
+
+# 获取用户输入十进制
+dec = int(input('请输入数字： '))
+
+print('十进制数为： ', dec)
+print('转换为二进制数为： ', bin(dec))
+print('转换为八进制数为： ', oct(dec))
+print('转换为十六进制数为： ', hex(dec))
