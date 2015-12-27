ffbd50b781179d1f0cb4f6b9e85cca50fec79877
One Zero <zeroonegit@gmail.com>
2015-12-27 14:58:09 +0800

    随机数生成

---
+++ random2.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# author by: One Zero
+# Filename: random.py
+
+# 生成 0 ~ 9 之间的随机数
+
+# 导入 random(随机数) 模块
+from random import randint
+
+print(randint(0, 9))
