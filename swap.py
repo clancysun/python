d884f8f988d3927f29e318a696174e6634bc1143
One Zero <zeroonegit@gmail.com>
2015-12-27 17:42:14 +0800

    不使用临时变量

--- swap.py
+++ swap.py
@@ -12,10 +12,13 @@
 x = input('输入 x 值: ')
 y = input('输入 y 值: ')
 
-# 创建临时变量，并交换
-temp = x
-x = y
-y = temp
+## 创建临时变量，并交换
+#temp = x
+#x = y
+#y = temp
+
+# 不使用临时变量
+x, y = y, x
 
 print('交换后 x 的值为： {}'.format(x))
 print('交换后 y 的值为： {}'.format(y))
