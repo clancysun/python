31aa1b5cdb7aa8e776e25d467b8655a06adcbc3a
One Zero <zeroonegit@gmail.com>
2015-12-27 20:55:50 +0800

    获取指定期间内的阿姆斯特朗数

--- number4.py
+++ number4.py
@@ -16,25 +16,81 @@
 
 # Python 检测用户输入的数字是否为阿姆斯特朗数
 
+
+#=======================================================================
+
+## 获取用户输入的数字
+#num = int(input('请输入一个数字： '))
+#
+## 初始化变量 sum
+#sum = 0
+#
+## 指数
+#n = len(str(num))
+#
+## 检测
+#temp = num
+#while temp > 0:
+#    digit = temp % 10
+#    sum += digit ** n
+#    temp //= 10
+#
+## 输出结果
+#if num == sum:
+#    print(num, '是阿姆斯特朗数')
+#else:
+#    print(num, '不是阿姆斯特朗数')
+
+
+#=======================================================================
+
+## 获取用户输入的数字
+##num = int(input('请输入一个数字： '))
+#
+#def amstl_number(num):
+#    # 初始化变量 sum
+#    sum = 0
+#
+#    # 指数
+#    n = len(str(num))
+#
+#    # 检测
+#    temp = num
+#    while temp > 0:
+#        digit = temp % 10
+#        sum += digit ** n
+#        temp //= 10
+#
+#    # 输出结果
+#    if num == sum:
+#        print(num, '是阿姆斯特朗数')
+#    #else:
+#    #    print(num, '不是阿姆斯特朗数')
+#
+#for i in range(1000):
+#    amstl_number(i)
+
+
+#=======================================================================
+
 # 获取用户输入的数字
-num = int(input('请输入一个数字： '))
+lower = int(input('最小值： '))
+upper = int(input('最大值： '))
 
-# 初始化变量 sum
-sum = 0
+for num in range(lower, upper + 1):
+    # 初始化变量 sum
+    sum = 0
 
-# 指数
-n = len(str(num))
+    # 指数
+    n = len(str(num))
 
-# 检测
-temp = num
-while temp > 0:
-    digit = temp % 10
-    sum += digit ** n
-    temp //= 10
+    # 检测
+    temp = num
+    while temp > 0:
+        digit = temp % 10
+        sum += digit ** n
+        temp //= 10
 
-# 输出结果
-if num == sum:
-    print(num, '是阿姆斯特朗数')
-else:
-    print(num, '不是阿姆斯特朗数')
-
+    # 输出结果
+    if num == sum:
+        print(num, '是阿姆斯特朗数')
