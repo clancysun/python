356759445c1c92c443cd47f41c7a458e2a66e538
One Zero <zeroonegit@gmail.com>
2015-12-28 01:18:04 +0800

    字符串大小写转换

---
+++ upper_lower_convert.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+############################
+# File Name: upper_lower_convert.py
+# Author: One Zero
+# Mail: zeroonegit@gmail.com
+# Created Time: 2015-12-28 01:10:28
+############################
+
+str = 'www.zeroonecloud.com'
+print(str.upper())                      # 把所有字符中的小写字母转换成大写字母
+print(str.lower())                      # 把所有字符中的大写字母转换成小写字母
+print(str.capitalize())                 # 把第一个字母转化为大写字母，其余小写
+print(str.title())                      # 把每个单词的第一个字母转化为大写，其余小写
