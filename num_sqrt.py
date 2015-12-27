3c3277dbcf106db3161671832daeb31378ce3e63
One Zero <zeroonegit@gmail.com>
2015-12-27 14:13:50 +0800

    计算正数的平方根

---
+++ num_sqrt.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# author by: One Zero
+# Filename: num_sqrt.py
+
+num = float(input('请输入一个数字： '))
+num_sqrt = num ** 0.5
+print(' %0.3f 的平方根为 %0.3f' % (num, num_sqrt))
