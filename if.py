22af673b68feacf2cf3170903aca98411f6a5c68
One Zero <zeroonegit@gmail.com>
2015-12-27 17:50:52 +0800

    使用内嵌if语句

--- if.py
+++ if.py
@@ -10,9 +10,18 @@
 # 用户输入数字
 
 num = float(input('输入一个数字： '))
-if num > 0:
-    print('正数')
-elif num == 0:
-    print('零')
+#if num > 0:
+#    print('正数')
+#elif num == 0:
+#    print('零')
+#else:
+#    print('负数')
+
+# 内嵌if语句
+if num >= 0:
+    if num == 0:
+        print('零')
+    else:
+        print('正数')
 else:
     print('负数')
