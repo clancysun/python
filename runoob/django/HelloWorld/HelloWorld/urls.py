5d1980f85b00cac3606a97e0de658064858944da
One Zero <zeroonegit@gmail.com>
2016-01-01 23:35:14 +0800

    Django模型

--- runoob/django/HelloWorld/HelloWorld/urls.py
+++ runoob/django/HelloWorld/HelloWorld/urls.py
@@ -1,4 +1,5 @@
-from django.conf.urls import *
+from django.conf urls import *
 from HelloWorld.view import hello
+from HelloWorld.testdb import testdb
 
-urlpatterns = patterns("", ('^hello/$', hello),)
+urlpatterns = patterns("", ('^hello/$', hello), ('^testdb/$', testdb),)
