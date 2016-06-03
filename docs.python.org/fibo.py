3e11caa5c550c4900a3bfc465a317d6c3fda5f1a
Quincey Sun <zeroonegit@gmail.com>
2016-06-03 22:19:02 +0800

    add conding:utf-8

--- docs.python.org/fibo.py
+++ docs.python.org/fibo.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
 # Fibonacci numbers module
 
 def fib(n):    # write Fibonacci series up to n
@@ -7,7 +9,6 @@ def fib(n):    # write Fibonacci series up to n
         a, b = b, a+b
     print()
 
-
 def fib2(n):    # return Fibonacci series up to n
     result = []
     a, b = 0, 1
@@ -15,3 +16,7 @@ def fib2(n):    # return Fibonacci series up to n
         result.append(b)
         a, b = b, a+b
     return result
+
+if __name__ == "__main__":
+    import sys
+    fib(int(sys.argv[1]))
