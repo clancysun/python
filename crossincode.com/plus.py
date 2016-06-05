f59a2b37726cc520b23566f89ba67504da85170c
Quincey Sun <zeroonegit@gmail.com>
2016-06-05 20:57:36 +0800

    函数的参数

---
+++ crossincode.com/plus.py
@@ -0,0 +1,8 @@
+def plus(num1, num2):
+    print('%d + %d = %d' % (num1, num2, num1+num2))
+
+if __name__ == '__main__':
+    num1 = int(input("Please input num1: "))
+    num2 = int(input("Please input num2: "))
+
+    plus(num1, num2)
