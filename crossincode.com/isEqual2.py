6baad267b139c4ca2ce5f376085e491bdc58a928
Quincey Sun <zeroonegit@gmail.com>
2016-06-05 21:13:58 +0800

    if, elif, else

---
+++ crossincode.com/isEqual2.py
@@ -0,0 +1,20 @@
+def isEqual(num1, num2):
+    if num1 < num2:
+        print('too small')
+        return False
+    if num1 > num2:
+        print('too big')
+        return False
+    if num1 == num2:
+        print('bingo')
+        return True
+
+from random import randint
+num = randint(1, 100)
+
+print('Guess what I think? ')
+bingo = False
+
+while bingo == False:
+   answer = input()
+   bingo = isEqual(answer, num)
