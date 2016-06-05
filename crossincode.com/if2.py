e9b150ec6e902653e2eae16d003f7e6b9722250e
Quincey Sun <zeroonegit@gmail.com>
2016-06-05 21:20:35 +0800

    if的嵌套

---
+++ crossincode.com/if2.py
@@ -0,0 +1,13 @@
+x = input('Please input x: ')
+y = input('Please input y: ')
+
+if y >= 0:
+    if x >= 0:
+        print('first')
+    else:
+        print('two')
+else:
+    if x < 0:
+        print('three')
+    else:
+        print('four')
