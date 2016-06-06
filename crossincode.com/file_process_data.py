b92fa605c84d6ecab827596ea8ba276b69ba4682
Quincey Sun <zeroonegit@gmail.com>
2016-06-06 10:43:15 +0800

    计算总成绩

---
+++ crossincode.com/file_process_data.py
@@ -0,0 +1,19 @@
+f = file('scores.txt')
+lines = f.readlines()
+f.close()
+
+results = []
+
+for line in lines:
+    data = line.split()
+    
+    sum = 0
+    for score in data[1:]:
+        sum += int(score)
+    result = '%s \t: %d\n' % (data[0], sum)
+
+    results.append(result)
+
+output = file('result.txt', 'w')
+output.writelines(results)
+output.close()
