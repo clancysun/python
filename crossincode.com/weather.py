535b08dd000557cc1c10cd33f2cde3803483bf8b
Quincey Sun <zeroonegit@gmail.com>
2016-06-09 21:46:37 +0800

    查天气

--- crossincode.com/weather.py
+++ crossincode.com/weather.py
@@ -6,6 +6,18 @@ from city import city
 cityname = raw_input('你想查哪个城市的天气？\n')
 citycode = city.get(cityname)
 if citycode:
-    url = ('http://www.weather.com.cn/data/cityinfo/%s.html' % citycode)
-    content = urllib2.urlopen(url).read()
-    print content
+    try:
+        url = ('http://www.weather.com.cn/data/cityinfo/%s.html' % citycode)
+        content = urllib2.urlopen(url).read()
+        data = json.loads(content)
+        result = data['weatherinfo']
+        str_temp = ('%s\n%s  ~ %s') % (
+            result['weather'],
+            result['temp1'],
+            result['temp2']
+            )
+        print str_temp
+    except:
+        print '查询失败'
+else:
+    print '没有找到该城市'
