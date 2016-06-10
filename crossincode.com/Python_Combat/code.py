ee6f26e734c1d7b6be0a3478aa0aa548a7a2afc6
Quincey Sun <zeroonegit@gmail.com>
2016-06-10 17:13:26 +0800

    使用dict作为数据源

--- crossincode.com/Python_Combat/code.py
+++ crossincode.com/Python_Combat/code.py
@@ -6,8 +6,22 @@ urls = (
 
 class index:
     def GET(self):
-        return "Hello, world!"
+        page = ''
+        for m in movies:
+            page += '%s (%d)\n' % (m['title'], m['year'])
+        return page
 
 if __name__ == "__main__":
     app = web.application(urls, globals())
     app.run()
+
+movies = [
+        {
+            'title': 'Forrest Gump',
+            'year': 1994,
+            },
+        {
+            'title': 'Titanic',
+            'year': 1997,
+            },
+        ]
