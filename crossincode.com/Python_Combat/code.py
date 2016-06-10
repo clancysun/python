5381ff81966a04a67e916f5d82f1140afadb2daa
Quincey Sun <zeroonegit@gmail.com>
2016-06-10 17:56:37 +0800

    更多的页面

--- crossincode.com/Python_Combat/code.py
+++ crossincode.com/Python_Combat/code.py
@@ -2,6 +2,7 @@ import web
 
 urls = (
     '/', 'index',
+    '/movie/(\d+)', 'movie',
 )
 
 render = web.template.render('templates/')
@@ -12,6 +13,14 @@ class index:
     def GET(self):
         movies = db.select('movie')
         return render.index(movies)
+
+class movie:
+    def GET(self, movie_id):
+        #movie_id = int(movie_id)
+        #movie = db.select('movie', where='id=$movie_id', vars=locals())[0]
+        condition = 'id=' + movie_id
+        movie = db.select('movie', where=condition)[0]
+        return render.movie(movie)
 
 if __name__ == "__main__":
     app = web.application(urls, globals())
