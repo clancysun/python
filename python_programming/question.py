3a99815c749bfd9eea1a5c907c3ae2a0605d8b89
Quincey Sun <zeroonegit@gmail.com>
2016-06-17 13:49:33 +0800

    Strings and Variables

---
+++ python_programming/question.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+question = "What did you have for lunch?"
+print (question)
+answer = input()    #You should use "input()" in python 3.x, because python 3.x
+                    #doesn't have a function named "raw_input".
+print ("You had " + answer + "! That sounds delicious!")
