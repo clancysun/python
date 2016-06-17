e77370c7d53f64f11c3f4bf13ae49b90b808c3e1
Quincey Sun <zeroonegit@gmail.com>
2016-06-17 16:40:11 +0800

    repeat string many times

---
+++ python_programming/string_repeat_many_times.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+###############################################################################
+# Author: Quincey Sun
+# Mail: zeroonegit@gmail.com
+# Created Time: 2016-06-17 16:36:33
+###############################################################################
+
+userStr = input("Please input a repeat string: \n")
+userNum = input("Please input a times: \n")
+
+print (userStr * int(userNum))
