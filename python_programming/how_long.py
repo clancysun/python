2b50b9ed36e79361ee5a147b83564fc13e8e2e0c
Quincey Sun <zeroonegit@gmail.com>
2016-06-17 16:35:19 +0800

    len()

---
+++ python_programming/how_long.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+###############################################################################
+# Author: Quincey Sun
+# Mail: zeroonegit@gmail.com
+# Created Time: 2016-06-17 16:28:06
+###############################################################################
+
+userInput = input("Please input a string: ")
+length = len(userInput)
+
+print("You input string length is {0}".format(length))
