295ccedefb5c544f24e845308a5d8a529ff4c06f
Quincey Sun <zeroonegit@gmail.com>
2016-06-17 14:31:39 +0800

    Combining Numbers and Strings

---
+++ python_programming/combining_numbers_and_strings.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+###############################################################################
+# Author: Quincey Sun
+# Mail: zeroonegit@gmail.com
+# Created Time: 2016-06-17 14:21:38
+###############################################################################
+
+print ("Please give me a number: ")
+response = input()
+
+number = int(response)
+plusTen = number + 10
+print ("If we add 10 to your number, we get " + str(plusTen))
