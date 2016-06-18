3f497f2959afddf4c4c77d653b4f390995abbe3f
Quincey Sun <zeroonegit@gmail.com>
2016-06-18 22:44:43 +0800

    ord()

---
+++ python_programming/stringparser.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+###############################################################################
+# Author: Quincey Sun
+# Mail: zeroonegit@gmail.com
+# Created Time: 2016-06-18 22:41:36
+###############################################################################
+
+s = input("Enter value: ")
+
+l1 = [c for c in s]
+l2 = [ord(c) for c in s]
+
+print(l1)
+print(l2)
