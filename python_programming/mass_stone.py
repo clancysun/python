c78fd3557fb5f39e3bf8a817ebf225692fd24758
Quincey Sun <zeroonegit@gmail.com>
2016-06-19 14:50:24 +0800

    mass stone

---
+++ python_programming/mass_stone.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+###############################################################################
+# Author: Quincey Sun
+# Mail: zeroonegit@gmail.com
+# Created Time: 2016-06-19 12:07:23
+###############################################################################
+
+mass_kg = int(input("What is your mass in kilograms? "))
+mass_stone = mass_kg * 2.2 / 14
+print("You weigh", mass_stone, "stone.")
