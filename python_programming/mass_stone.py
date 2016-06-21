0943572bad79f05f8ae313ab1f2a1e307eb209fb
Quincey Sun <zeroonegit@gmail.com>
2016-06-21 22:15:07 +0800

    print the result to two decimal places.

--- python_programming/mass_stone.py
+++ python_programming/mass_stone.py
@@ -8,4 +8,4 @@
 
 mass_kg = int(input("What is your mass in kilograms? "))
 mass_stone = mass_kg * 2.2 / 14
-print("You weigh", mass_stone, "stone.")
+print("You weigh", round(mass_stone, 2), "stone.")
